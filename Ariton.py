b032f73a9b74ae80cff200b3cb1d892729847630
Darkrider123 <cosmin.ariton@gmai.com>
2020-06-15 08:54:10 +0300

    COCO

--- Ariton.py
+++ Ariton.py
@@ -13,9 +13,14 @@ miscadin_buric='Sa te mai pup un pic'
 
 def calul_sare():
         print("Dafuq? Te-a sarit calul?")
-        
-
-Laptele_Rosu_E_De_Femeia_Batuta='Adevarat'
 
 
-cepulamea=2
+
+ogjsapoj=944
+
+
+def branza:
+        lapte = "Scump"
+
+
+canapea = 0
