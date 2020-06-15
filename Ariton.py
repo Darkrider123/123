0844b2444790545809db0bf473ee7346cf3253d5
Darkrider123 <cosmin.ariton@gmai.com>
2020-06-15 07:17:34 +0300

    Dani Mocanu la putere

--- Ariton.py
+++ Ariton.py
@@ -5,9 +5,17 @@ print ("COCO")
 
 def sare_calul():
         print("Hopa, ai sarit calul")
+        RupemBaraka="DaniMocanuTrageDeFiare"
+        print(RupemBaraka)
 
+miscadin_buric='Sa te mai pup un pic'
 
 
 def calul_sare():
         print("Dafuq? Te-a sarit calul?")
         
+
+Laptele_Rosu_E_De_Femeia_Batuta='Adevarat'
+
+
+cepulamea=2
